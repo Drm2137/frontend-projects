898fa4546e0cc47553890316fbfe6fa87b1f9469
Drm2137 <makhil01@gmail.com>
2025-10-27 13:15:40 -0500

    Add app.js file

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import logo from  './logo.svg';
+import './App.css';
+
+function App() {
+    const name = "Akhil";
+    return (
+        <><h1 className="container"> My name is Akhil</h1>
+        <button>Click Me</button></>
+    );
+}   
+export default App;
